48018eecab7193c5232cb3661ace0c8f99197282
Mubelotix <mubelotix@gmail.com>
2025-10-29 12:42:48 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ normal='\033[0m'
 
 # This script uses the Rustup script as a library to detect the architecture of the system
 source_rustup_functions() {
-    echo "[1/3] Downloading library"
+    echo "[1/7] Downloading library"
     rust_script=$(curl --proto '=https' --tlsv1.2 -sS https://sh.rustup.rs)
     if [ $? -ne 0 ]; then
         echo "${red}Error: Failed to download library${normal}"
@@ -32,16 +32,44 @@ latest_url=$(curl -sSL -w "%{url_effective}" -o /dev/null "https://github.com/Mu
 version=$(echo "$latest_url" | sed 's:.*/::')
 download_url="https://github.com/mubelotix/nginx-hibernator/releases/download/$version/nginx-hibernator_$arch"
 
-echo "[2/3] Downloading nginx-hibernator $version"
+echo "[2/7] Downloading nginx-hibernator backend $version"
 curl --fail --location --progress-bar "$download_url" -o "/tmp/$filename"
 case $? in
     0)  ;;
-    22) echo "${red}No available binary for your system ($arch).${normal} Please build from source: https://github.com/Mubelotix/nginx-hibernator"; exit 1 ;;
-    *)  echo "${red}Error: Failed to download nginx-hibernator binary${normal}"; exit 1 ;;
+    22) echo "${red}No available backend binary for your system ($arch).${normal} Please build from source: https://github.com/Mubelotix/nginx-hibernator"; exit 1 ;;
+    *)  echo "${red}Error: Failed to download nginx-hibernator backend${normal}"; exit 1 ;;
 esac
 
-echo "[3/3] Installing nginx-hibernator at /usr/local/bin/nginx-hibernator"
+echo "[3/7] Downloading nginx-hibernator frontend $version"
+download_url="https://github.com/mubelotix/nginx-hibernator/releases/download/$version/frontend.zip"
+curl --fail --location --progress-bar "$download_url" -o "/tmp/frontend.zip"
+case $? in
+    0)  ;;
+    22) echo "${red}Frontend not found for version $version.${normal} Please build from source: https://github.com/Mubelotix/nginx-hibernator"; exit 1 ;;
+    *)  echo "${red}Error: Failed to download nginx-hibernator frontend${normal}"; exit 1 ;;
+esac
+
+echo "[4/7] Downloading nginx-hibernator default landing page $version"
+download_url="https://github.com/mubelotix/nginx-hibernator/releases/download/$version/landing.zip"
+curl --fail --location --progress-bar "$download_url" -o "/tmp/landing.zip"
+case $? in
+    0)  ;;
+    22) echo "${red}Landing page not found for version $version.${normal} Please build from source: https://github.com/Mubelotix/nginx-hibernator"; exit 1 ;;
+    *)  echo "${red}Error: Failed to download nginx-hibernator landing page${normal}"; exit 1 ;;
+esac
+
+echo "[5/7] Installing nginx-hibernator at /usr/local/bin/nginx-hibernator"
 sudo mv "/tmp/$filename" "/usr/local/bin/nginx-hibernator"
 chmod +x "/usr/local/bin/nginx-hibernator"
+
+echo "[6/7] Installing nginx-hibernator frontend at /usr/share/nginx/html/nginx-hibernator-frontend"
+sudo unzip -o "/tmp/frontend.zip" -d "/usr/share/nginx/html/nginx-hibernator-frontend"
+sudo chown -R www-data:www-data "/usr/share/nginx/html/nginx-hibernator-frontend"
+sudo chmod -R 755 "/usr/share/nginx/html/nginx-hibernator-frontend"
+
+echo "[7/7] Installing nginx-hibernator default landing page at /usr/share/nginx/html/nginx-hibernator-landing"
+sudo unzip -o "/tmp/landing.zip" -d "/usr/share/nginx/html/nginx-hibernator-landing"
+sudo chown -R www-data:www-data "/usr/share/nginx/html/nginx-hibernator-landing"
+sudo chmod -R 755 "/usr/share/nginx/html/nginx-hibernator-landing"
 
 echo "${green}nginx-hibernator $version has been installed successfully!${normal}"
